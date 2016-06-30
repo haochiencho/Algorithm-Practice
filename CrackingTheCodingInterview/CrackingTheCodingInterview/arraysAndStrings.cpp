149e1d725a8d442a67e720a927e32ac0065e617e
Brian Cho <haochiencho@gmail.com>
2016-06-30 14:07:12 -0700

    update arrayAndStrings.cpp

--- CrackingTheCodingInterview/CrackingTheCodingInterview/arraysAndStrings.cpp
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/arraysAndStrings.cpp
@@ -9,9 +9,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
-// # 1
 
 bool isUnique(string word){
     string prevChars = "";
@@ -90,10 +91,93 @@ bool isPalindrome(string a){ // is a word or phrase a palindrome
     
 }
 
+unsigned int min(unsigned int a, unsigned int b){
+    if(a > b)
+        return b;
+    else
+        return a;
+}
+
+bool isOneEditDistance(string a, string b){ // checks if the two strings are one edit distance from each other
+    if((int)a.length() - (int)b.length() < -1 || (int)a.length() - (int)b.length() > 1)
+        return false;
+    bool firstEdit = false;
+    int Ashift = 0;
+    int Bshift = 0;
+    for(int i = 0; i < min(a.length(), b.length()); i++){
+        if(a[i + Ashift] != b[i + Bshift]){
+            if(firstEdit)
+                return false;
+            else{
+                if(a.length() == b.length()){
+                    firstEdit = true;
+                    continue;
+                }
+                else if(a.length() > b.length())
+                    Ashift++;
+                else
+                    Bshift++;
+                firstEdit = true;
+            }
+        }
+    }
+    return true;
+}
+
+string compressString(string a){ // compress string aaabbc to a3b2c1
+    string *output = new string;
+    if(a.length() == 0)
+        return *output;
+    
+    char temp = a[0];
+    int count = 1;
+    string tempHolder = "";
+    string countHolder;
+    for(int i = 1; i < a.length(); i++){
+        if(temp == a[i])
+            count++;
+        else{
+            temp = a[i - 1];
+            tempHolder += temp;
+            output->append(tempHolder);
+            countHolder = to_string(count);
+            output->append(countHolder);
+            count = 1;
+            tempHolder.clear();
+            temp = a[i];
+        }
+    }
+    tempHolder += a[a.length() - 1];
+    output->append(tempHolder);
+    countHolder = to_string(count);
+    output->append(countHolder);
+    return *output;
+}
+
+void rotateMatrix(vector<vector<char> > & vect){ // rotate N x N matrix by 90 degrees
+    for(int i = 0; i < vect.size() / 2; i++){
+        for(int j = i; j < vect.size() - i - 1; j++){
+            char temp = vect[i][j];
+            vect[i][j] = vect[vect.size() - j - 1][i];
+            vect[vect.size() - j - 1][i] = vect[vect.size() - i - 1][vect.size() - j - 1];
+            vect[vect.size() - i - 1][vect.size() - j - 1] = vect[j][vect.size() - i - 1];
+            vect[j][vect.size() - i - 1] = temp;
+            
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    string a = "aba cc";
-    string b = "aabc";
-    std::cout << isPalindrome(a);
+    string a = "";
+    string b = "abaa c";
+    vector<vector<char> > vect;
+    for(int i = 0; i < 5; i++){
+        vector<char> temp;
+        for(int j = 0; j < 5; j++)
+            temp.push_back(static_cast<char>(j));
+        vect.push_back(temp);
+    }
+    rotateMatrix(vect);
     return 0;
 }
