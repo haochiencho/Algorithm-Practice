96e5bb95698c73cee7c8fb680eb65d1540004391
Brian Cho <haochiencho@gmail.com>
2016-08-12 11:53:26 -0700

    create object.cpp

---
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/object.cpp
@@ -0,0 +1,24 @@
+//
+//  object.cpp
+//  introductionToAlgorithmCormen
+//
+//  Created by Brian Cho on 8/12/16.
+//  Copyright © 2016 Cho. All rights reserved.
+//
+
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+using namespace std;
+
+
+int main(int argc, char* argv[]){
+    cout << "hello world!";
+}
