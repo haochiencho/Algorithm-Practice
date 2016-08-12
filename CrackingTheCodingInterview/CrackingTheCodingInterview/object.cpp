c53ada03748dc4b8b929147bf216cf27515720c3
Brian Cho <haochiencho@gmail.com>
2016-08-12 12:50:20 -0700

    update object.cpp

--- CrackingTheCodingInterview/CrackingTheCodingInterview/object.cpp
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/object.cpp
@@ -18,6 +18,94 @@
 #include <map>
 using namespace std;
 
+// design a card deck and implement black jack
+
+struct Card{ // assuming no jokers
+    string suit;
+    int value;
+};
+
+class Player{
+public:
+    Player(int money){
+        curMoney = money;
+    }
+    void placeBet(int bet);
+    void getCard(); // gets a random card
+    bool isBust(); // checks if the player has busted
+    int countSum();
+private:
+    vector<Card> hand;
+    int curMoney;
+    int curBet;
+};
+
+class BlackJack{
+public:
+    BlackJack(vector<Player> vect) : dealer(INT_MAX){
+        players = vect;
+    }
+    void dealCards(); // deals cards to all players and the dealer
+    bool comparedWithDealer(Player player); // compares to see if player wins
+private:
+    Player dealer;
+    vector<Player> players;
+};
+
+void Player::placeBet(int bet){
+    curBet = bet;
+}
+
+void Player::getCard(){
+    Card* card = new Card;
+    card->value = (rand() % 13) + 1;
+    card->suit = rand() % 4;
+    hand.push_back(*card);
+}
+
+int Player::countSum(){
+    int sum = 0;
+    int aceCount = 0;
+    for(int i = 0; i < hand.size(); i++){
+        if(hand[i].value == 1){
+            sum += 11;
+            aceCount++;
+        }
+        else
+            sum += hand[i].value;
+    }
+    while(aceCount > 0 && sum > 21){
+        sum -= 10;
+        aceCount--;
+    }
+    return sum;
+}
+
+bool Player::isBust(){
+    if(countSum() > 21)
+        return true;
+    else
+        return false;
+}
+
+void BlackJack::dealCards(){
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < players.size(); j++){
+            players[j].getCard();
+        }
+        dealer.getCard();
+    }
+}
+
+bool BlackJack::comparedWithDealer(Player player){
+    if(dealer.countSum() > player.countSum())
+        return false;
+    else
+        return true;
+}
+
+
+
 
 int main(int argc, char* argv[]){
     cout << "hello world!";
