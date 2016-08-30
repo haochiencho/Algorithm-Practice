9a017bfa8a40d12319b8f8aff6735314fd1a5135
Brian Cho <haochiencho@gmail.com>
2016-08-30 15:33:29 -0700

    update sortingAndSearching.cpp

--- CrackingTheCodingInterview/CrackingTheCodingInterview/sortingAndSearching.cpp
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/sortingAndSearching.cpp
@@ -47,10 +47,43 @@ vector<int> merge(vector<int> A, vector<int> B){
     return output;
 }
 
+// given a vector of string, place all anagrams next to each other
+
+bool isAnagram(string A, string B){
+    if(A.length() != B.length())
+        return false;
+    vector<int> hashA(26, 0);
+    vector<int> hashB(26, 0);
+    for(int i = 0; i < A.length(); i++){
+        hashA[A[i] - 'a']++;
+        hashB[B[i] = 'a']++;
+    }
+    for(int i = 0; i < 26; i++){
+        if(hashA[i] != hashB[i])
+            return false;
+    }
+    return true;
+}
+
+void anagram(vector<string> & vect){
+    for(int i = 1; vect.size(); i++){
+        for(int j = 0; j < i; j++){
+            if(isAnagram(vect[i], vect[j])){
+                vect.insert(vect.begin() + j, vect[i]);
+                vect.erase(vect.begin() + i);
+                break;
+            }
+        }
+    }
+}
+
+
 int main(int argc, char* argv[]){
+
     int arr1[] = {1, 3, 5};
     int arr2[] = {1, 2, 4, 6};
     vector<int> A(arr1, arr1 + sizeof(arr1) / sizeof(int));
     vector<int> B(arr2, arr2 + sizeof(arr2) / sizeof(int));
     vector<int> temp = merge(A, B);
+    
 }
