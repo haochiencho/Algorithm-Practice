d5d0fdfd66609c7062a15d0fbbd500495d557928
Brian Cho <haochiencho@gmail.com>
2016-09-13 20:47:50 -0700

    update moderate.cpp

--- CrackingTheCodingInterview/CrackingTheCodingInterview/moderate.cpp
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/moderate.cpp
@@ -113,9 +113,127 @@ vector<int> intersect(int x1, int y1, int x2, int y2, int X1, int Y1, int X2, in
     return result;
 }
 
+// given NxN matrix. look for N sequence of 1s
+
+bool detectLine(vector<vector<int> > vect){
+    int n = vect.size();
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){ // check rows
+            if(vect[i][j] == '0')
+                break;
+            if(j == n - 1)
+                return true;
+        }
+    }
+    for(int i = 0; i < n; i++){ // check cols
+        for(int j = 0; j < n; j++){
+            if(vect[j][i] == '0')
+                break;
+            if(j == n - 1)
+                return true;
+        }
+    }
+    for(int i = 0; i < n; i++){ // check diagonals
+        if(vect[i][i] == '0')
+            break;
+        if(i == n - 1)
+            return true;
+    }
+    for(int i = 0; i < n; i++){
+        if(vect[i][n - i - 1] == '0')
+            break;
+        if(i == n - 1)
+            return true;
+    }
+    return false;
+}
+
+// number of trailing zeroes in n factorial
+
+int numZeroes(int factorial){
+    int count = 0;
+    int numInc = 0;
+    int remain;
+    for(int i = 5; i <= factorial; i += 5){
+        remain = i;
+        numInc = 0;
+        while(remain % 5 == 0){
+            numInc++;
+            remain /= 5;
+        }
+        count += numInc;
+    }
+    return count;
+}
+
+// given two vectors of integers output the pair with the smallest difference
+
+vector<int> sort(vector<int> vect, int start, int end){
+    vector<int> temp;
+    if(start >= end){
+        temp.push_back(vect[start]);
+        return temp;
+    }
+    int mid;
+    mid = (start + end) / 2;
+    vector<int> left = sort(vect, start, mid);
+    vector<int> right = sort(vect, mid + 1, end);
+    int i, j;
+    i = 0;
+    j = 0;
+    while(i + j < left.size() + right.size()){
+        if(i >= left.size()){
+            temp.push_back(right[j]);
+            j++;
+            continue;
+        }
+        if(j >= right.size()){
+            temp.push_back(left[i]);
+            i++;
+            continue;
+        }
+        if(left[i] > right[j])
+            temp.push_back(right[i]);
+        else
+            temp.push_back(left[i]);
+    }
+    return temp;
+}
+
+int abs(int a, int b){
+    if(a > b)
+        return a - b;
+    else
+        return b - a;
+}
+
+vector<int> smallestPair(vector<int> A, vector<int> B){
+    vector<int> sortedA = sort(A, 0, A.size() - 1);
+    vector<int> sortedB = sort(B, 0 , B.size() - 1);
+    vector<int> result(2);
+    int curMin = INT_MAX;
+    int i, j;
+    i = 0;
+    j = 0;
+    while(i < sortedA.size() && j < sortedB.size()){
+        if(abs(sortedA[i], sortedB[j]) > curMin){
+            curMin = abs(sortedA[i], sortedB[j]);
+            result[0] = sortedA[i];
+            result[1] = sortedB[j];
+        }
+        if(sortedA[i] > sortedB[j])
+            j++;
+        else
+            i++;
+    }
+    return result;
+}
+
 int main(int argc, char* argv[]){
     int a = 5;
     int b = 9;
     swap(a, b);
     intersect(0, 0, 1, 1, 2, 0, 1, 1);
+    numZeroes(30);
+    
 }
